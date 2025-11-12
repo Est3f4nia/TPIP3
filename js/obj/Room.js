fd669cc71694dd99ae04fb22db65577b45b20de5
Estefanía Villarreal <villarrealestefania758@gmail.com>
2025-11-12 11:43:17 -0300

    Create Room.js

---
+++ js/obj/Room.js
@@ -0,0 +1,8 @@
+class Room {
+    constructor(id, tipo, precio, disponible) {
+        this.id = id;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+}
