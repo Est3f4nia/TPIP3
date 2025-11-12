9c0dc6cc15d3324348fc51cb8c9992f7c8b58d89
Estefanía Villarreal <villarrealestefania758@gmail.com>
2025-11-12 11:43:49 -0300

    Create Reservation.js

---
+++ js/obj/Reservation.js
@@ -0,0 +1,10 @@
+class Reservation {
+    constructor(id, userId, roomId, checkIn, checkOut, estado) {
+        this.id = id;
+        this.userId = userId;
+        this.roomId = roomId;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.estado = estado
+    }
+}
