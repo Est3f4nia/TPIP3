6c69606d402cf1f81e6e70067b2a18373bd1f84e
Estefanía Villarreal <villarrealestefania758@gmail.com>
2025-11-18 23:45:10 -0300

    Add estado getter and setter to Reservation class

--- js/obj/Reservation.js
+++ js/obj/Reservation.js
@@ -5,6 +5,29 @@ class Reservation {
         this.roomId = roomId;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        this.estado = estado
+        this.estado = estado;
+    }
+
+    getEstado() {
+        return this.estado;
+    }
+
+    setEstado(nuevoEstado) {
+        if (!['Confirmada', 'Cancelada'].includes(nuevoEstado)) throw new Error('Estado inválido');
+        this.estado = nuevoEstado;
+    }
+
+    // para stringify en fetch (puede servir?)
+    toJSON() {
+        return {
+            id: this.id,
+            userId: this.userId,
+            roomId: this.roomId,
+            checkIn: this.checkIn,
+            checkOut: this.checkOut,
+            estado: this.estado
+        };
     }
 }
+
+export { Reservation };
