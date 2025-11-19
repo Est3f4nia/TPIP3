8f923a5d90e6e9b5847c6b59fc7080054d884855
Estefanía Villarreal <villarrealestefania758@gmail.com>
2025-11-18 23:47:03 -0300

    Add validations for precio and disponible in Room class

--- js/obj/Room.js
+++ js/obj/Room.js
@@ -4,5 +4,30 @@ class Room {
         this.tipo = tipo;
         this.precio = precio;
         this.disponible = disponible;
+
+        // Validaciones
+        if (typeof precio !== 'number' || precio <= 0) throw new Error('Precio debe ser un número positivo');
+        if (typeof disponible !== 'boolean') throw new Error('Disponible debe ser booleano (true/false)');
+    }
+
+    getPrecio() {
+        return this.precio;
+    }
+
+    setPrecio(nuevoPrecio) {
+        if (typeof nuevoPrecio !== 'number' || nuevoPrecio <= 0) throw new Error('Precio debe ser un número positivo');
+        this.precio = nuevoPrecio;
+    }
+
+    // para stringify en fetch (puede servir?)
+    toJSON() {
+        return {
+            id: this.id,
+            tipo: this.tipo,
+            precio: this.precio,
+            disponible: this.disponible
+        };
     }
 }
+
+export { Room };
