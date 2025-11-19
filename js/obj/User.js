363f4231cc8b1e95ca4858557ae14f0677bf2a48
Estefanía Villarreal <villarrealestefania758@gmail.com>
2025-11-18 23:46:33 -0300

    Refactor User class to include password handling

--- js/obj/User.js
+++ js/obj/User.js
@@ -1,8 +1,25 @@
 class User {
-    #pass;
-    constructor(nombre, email) {
+    #password; 
+    constructor(id, nombre, email, password, role) {
+        this.id = id;  // ID único de MockAPI
         this.nombre = nombre;
         this.email = email;
-        this.#pass = this.#pass;
+        this.#password = password;
+        this.role = role;  // 'ADMIN' o 'USUARIO'
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    verifyPassword(inputPassword) {
+        return this.#password === inputPassword;
+    }
+
+    // Método para cambiar password (solo para ADMIN, pero lo chequeamos en script.js)
+    changePassword(newPassword) {
+        this.#password = newPassword;
     }
 }
+
+export { User };
